ce1d311138399b97b997a2d250aa23428a712caa
Nic <nicolae.mihale@gmail.com>
2019-11-25 22:01:33 +0200

    Updates

--- index.php
+++ index.php
@@ -4,5 +4,8 @@
 //putenv('COMPOSER_HOME=/opt/cpanel/composer/bin');
 //exec('php-cli /opt/cpanel/composer/bin/composer install', $out);
 //var_dump($out);
-echo $_SERVER['HTTPS'];
+
+
+var_dump($_SERVER['HTTPS']);
+
 echo "Test";
