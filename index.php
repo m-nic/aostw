52836f39969bb847272b9906f8a19ebe78790d46
Nic <nicolae.mihale@gmail.com>
2019-12-01 17:07:39 +0200

    Add deploy config for heroku

--- index.php
+++ index.php
@@ -6,3 +6,17 @@
 //var_dump($out);
 
 echo "Test";
+
+if ($_GET['deploy_config']) {
+    file_put_contents(
+        'config.json',
+        <<<DELIM
+{
+  "database": {
+    "driver": "sqlite",
+    "db_name": "soa_demo"
+  }
+}
+DELIM
+    );
+}
